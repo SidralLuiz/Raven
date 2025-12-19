78869068b297a4f6d5b5e4086705e8dac6cc4d2b
SidralLuiz <lfsidral@gmail.com>
2025-12-18 22:01:24 -0300

    Subindo arquivo mysql

--- Db/raven.sql
+++ Db/raven.sql
@@ -1,2 +1,9 @@
-create database Estoque;
-use Estoque;adada
+create database raven;
+use raven;
+
+create table Users(
+UserId int primary key auto_increment,
+Name varchar(20),
+password  varchar(25)
+
+);
